e264ddf5ae0d42ec23ac35b350b83f1b216e3309
hasan75 <hasanjab14@gmail.com>
2025-04-24 20:02:58 +0600

    feat(landing): service linked on public

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'src/index.html')
+                main: resolve(__dirname, 'src/index.html'),
+                services: resolve(__dirname, 'src/services.html')
             }
         }
     },
